4442a37c0039b66681c1a9a9f74877549afcc4d2
breno romeiro <bsromeiro@gmail.com>
2020-05-08 20:49:08 -0300

    Updates

---
+++ precache-manifest.4ebf07ed9e430d718a79fbb1e37666bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38a94ce0f2f8169c0917c4364cba720",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "efb979acd3c599949c83",
+    "url": "/monsters-rolodex/static/css/main.3fb0e3da.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb979acd3c599949c83",
+    "url": "/monsters-rolodex/static/js/main.2d73148a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
